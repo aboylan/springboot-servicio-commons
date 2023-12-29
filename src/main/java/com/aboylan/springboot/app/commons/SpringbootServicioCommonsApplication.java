54bde372e103299055e0a85753f8733db0d6d084
Alex Boylan <aboylan@users.noreply.github.com>
2023-12-28 22:23:58 -0600

    Creando libreria commons y generando jar usando maven

--- src/main/java/com/aboylan/springboot/app/commons/SpringbootServicioCommonsApplication.java
+++ src/main/java/com/aboylan/springboot/app/commons/SpringbootServicioCommonsApplication.java
@@ -1,13 +1,8 @@
 package com.aboylan.springboot.app.commons;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootServicioCommonsApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootServicioCommonsApplication.class, args);
-	}
 
 }
